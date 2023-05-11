68ca52266274b61c53b938c6467357ca5a95d583
Jack Lukomski <jtlukomski@gmail.com>
2023-05-10 23:42:36 -0400

    added git ignore

--- Build/Libaries/NeuralC/Sources/Modules/ActivationFunctions/CMakeFiles/ActivationFunctions.dir/activationFunctions.c.obj.d
+++
@@ -1,9 +0,0 @@
-Libaries/NeuralC/Sources/Modules/ActivationFunctions/CMakeFiles/ActivationFunctions.dir/activationFunctions.c.obj: \
- C:\Users\jtluk\GitHub\MNIST_HandWrittenNumbersNN\Libaries\NeuralC\Sources\Modules\ActivationFunctions\activationFunctions.c \
- C:\Users\jtluk\GitHub\MNIST_HandWrittenNumbersNN\Libaries\NeuralC\Sources\Modules\ActivationFunctions\activationFunctions.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
- c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- c:\mingw\include\stdlib.h c:\mingw\include\math.h
--- Build/Libaries/NeuralC/Sources/Modules/LayerCreation/CMakeFiles/LayerCreation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LayerCreation.
-# This may be replaced when dependencies are built.
